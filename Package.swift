e038f97f2f72ea2c2f30613efa91052a54429cc4
bo1ta <calinbercea007@gmail.com>
2025-04-14 17:45:13 +0300

    Revert platforms changes

--- Package.swift
+++ Package.swift
@@ -3,27 +3,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "GoogleCloudLogging",
-    platforms: [.iOS(.v17)],
-    products: [
-        .library(
-            name: "GoogleCloudLogging",
-            targets: ["GoogleCloudLogging"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
-    ],
-    targets: [
-        .target(
-            name: "GoogleCloudLogging",
-            dependencies: [
-                .product(name: "Logging", package: "swift-log"),
-            ]
-        ),
-        .testTarget(
-            name: "GoogleCloudLoggingTests",
-            dependencies: ["GoogleCloudLogging"]
-        ),
-    ]
+  name: "GoogleCloudLogging",
+
+  platforms: [
+    .iOS(.v16),
+    .macOS(.v10_13),
+    .tvOS(.v13),
+    .watchOS(.v4),
+  ],
+  products: [
+    .library(
+      name: "GoogleCloudLogging",
+      targets: ["GoogleCloudLogging"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
+  ],
+  targets: [
+    .target(
+      name: "GoogleCloudLogging",
+      dependencies: [
+        .product(name: "Logging", package: "swift-log"),
+      ]
+    ),
+    .testTarget(
+      name: "GoogleCloudLoggingTests",
+      dependencies: ["GoogleCloudLogging"]
+    ),
+  ]
 )
